678e79f46bb2989956387f90e8b3e90bcadd1812
“LauraGA777” <laura_garciaa@soy.sena.edu.co>
2024-08-25 18:19:16 -0500

    Storage

--- projects/02-triki/src/App.jsx
+++ projects/02-triki/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import confetti from 'canvas-confetti'
 import { Square } from './components/Square'
 import { TURNS } from './constants'
 import { checkWinnerFrom, checkEndGame } from './login/board'
 import { WinnerModal } from './components/WinnerModal'
+import { saveGameToStorage, resetGameStorage } from './login/storage'
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+    
+  })
+  const [turn, setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+
+
   // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null)
 
@@ -17,6 +26,7 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
   }
   
 
@@ -31,6 +41,12 @@ function App() {
     // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
     // revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
@@ -40,6 +56,13 @@ function App() {
       setWinner(false) // empate
     }
   }
+
+  useEffect(()=>{
+    //Como minimo se ejecuta una vez
+    
+    console.log('useEffect')
+    // Si pasamos el array vacio, solo se ejecuta una vez cuando se renderiza el componente
+  },[winner])
 
   return (
     <main className='board'>
